501d30448aa9bcc10ac700352496d0f6c9cefeba
valdal14 <valdal14@gmail.com>
2020-11-15 19:21:09 +0100

    Completed the git section and added express to the project

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const serveStatic = require('serve-static');
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(serveStatic('./', { 'index': ['index.html'] }));
+
+app.listen(3000, ()=> {
+    console.log('Server started at port 3000');
+})
