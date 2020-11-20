5361a594c581f6e08716919d066e51933ce8fe45
valdal14 <valdal14@gmail.com>
2020-11-20 09:28:52 +0100

    Completed the Bootstrap section and started the Less and Saas section

--- 6-Boostrap-Training/scripts.js
+++ 6-Boostrap-Training/scripts.js
@@ -1,1 +1,9 @@
-$('[data-toggle="tooltip"]').tooltip();
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });
+});
