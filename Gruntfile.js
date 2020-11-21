7a90d3a9aa7d974be4414448f35f5240047af019
valdal14 <valdal14@gmail.com>
2020-11-21 19:18:19 +0100

    Completed the Task Runners Section

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,9 @@ module.exports = function (grunt) {
     require('time-grunt')(grunt);
 
     // Automatically load required Grunt tasks
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
 
     const sass = require('node-sass');
 
@@ -37,13 +39,158 @@ module.exports = function (grunt) {
                 options: {
                     watchTask: true,
                     server: {
-                        baseDir: "./6-Boostrap-Training/"
+                        baseDir: "./"
                     }
+                }
+            }
+        },copy: {
+            html: {
+                files: [
+                {
+                    //for html
+                    expand: true,
+                    dot: true,
+                    cwd: './6-Boostrap-Training',
+                    src: ['*.html', '*.css', '*.js'],
+                    dest: 'dist'
+                }]                
+            },
+            fonts: {
+                files: [
+                {
+                    //for font-awesome
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            },
+            images: {
+                files: [
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: './6-Boostrap-Training/img',
+                        src: ['*.png'],
+                        dest: 'dist/img'
+                    }
+                ]
+            }
+        },
+        clean: {
+            build: {
+                src: [ 'dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,                     // Enable dynamic expansion
+                    cwd: './6-Boostrap-Training',    // Src matches are relative to this path
+                    src: ['img/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'dist/'                  // Destination path prefix
+                }]
+            }
+        },useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html','aboutus.html','index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js:['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block) {
+                            var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        // Concat
+        concat: {
+            options: {
+                separator: ';'
+            },
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+
+        // Uglify
+        uglify: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+
+        cssmin: {
+            dist: {}
+        },
+        // Filerev
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+            release: {
+            // filerev:release hashes(md5) all assets (images, js and css )
+            // in dist directory
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css',
+                    ]
+                }]
+            }
+        },
+        // Usemin
+        // Replaces all assets with their revved version in html and css files.
+        // options.assetDirs contains the directories for finding the assets
+        // according to their relative paths
+        usemin: {
+            html: ['dist/contactus.html','dist/aboutus.html','dist/index.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css','dist/js']
+            }
+        },
+
+        htmlmin: {                                         // Task
+            dist: {                                        // Target
+                options: {                                 // Target options
+                    collapseWhitespace: true
+                },
+                files: {                                   // Dictionary of files
+                    'dist/index.html': 'dist/index.html',  // 'destination': 'source'
+                    'dist/contactus.html': 'dist/contactus.html',
+                    'dist/aboutus.html': 'dist/aboutus.html',
                 }
             }
         }
     });
 
+    grunt.registerTask('css', ['sass']);
     grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ]);
 
 };
