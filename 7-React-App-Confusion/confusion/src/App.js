bb058e87fc28581b0ba057ac3c88b3dcb3038ad1
valdal14 <valdal14@gmail.com>
2020-11-23 19:30:11 +0100

    Added bootsrap and react-bootstrap to the react training project

--- 7-React-App-Confusion/confusion/src/App.js
+++ 7-React-App-Confusion/confusion/src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Importing react-bootstrap
+import { Navbar, NavbarBrand } from 'react-bootstrap';
+
 function App() {
   return (
     <div className="App">
+      
+      {/* react-bootstrap NavBar */}
+      <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+      </Navbar>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
