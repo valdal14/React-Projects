83ecb08c8790b2b02d5d8bdba003f6b05df04b8e
valdal14 <valdal14@gmail.com>
2020-11-26 12:46:00 +0100

    Re-organized the project structure

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(bodyParser.json());
 
 app.use(serveStatic('./', { 'index': ['index.html'] }));
 
-app.listen(3000, ()=> {
-    console.log('Server started at port 3000');
+app.listen(3001, ()=> {
+    console.log('Server started at port 3001');
 })
