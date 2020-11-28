36dc7c985414f865073d007fefc37f29ca99ec41
valdal14 <valdal14@gmail.com>
2020-11-28 09:55:58 +0100

    Started the section about states

--- 9-React-Complete-Project/react-project/src/App.js
+++ 9-React-Complete-Project/react-project/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-     <Person />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* Using props inside Components */}
+//      <Person name="Valerio" age="42" />
+//      {/* Using Children props inside Components */}
+//      <Person name="Lefemas" age="45" > My Hobby is: Reading</Person>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  
+  render(){
+    return (
+      <div className="App">
+        {/* Using props inside Components */}
+       <Person name="Valerio" age="42" />
+       {/* Using Children props inside Components */}
+       <Person name="Lefemas" age="45" > My Hobby is: Reading</Person>
+      </div>
+    );
+  }
 }
 
 export default App;
--- 9-React-Complete-Project/react-project/src/Person/Person.js
+++ 9-React-Complete-Project/react-project/src/Person/Person.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
+const generateRandomNumber = () => { 
+    return Math.floor(Math.random() * 90) 
+}
+
 // React must be imported since the following will be converted in React.createElement and JSX
 // We do not need to import Component tho since we are not using a class
-const person = () => {
-    return <h2>I'am a <b>Functional Component</b></h2>
+
+// We must use props parameter inside our functional component to be able to create props.name and props.age
+const person = (props) => {
+    return (
+        <div>
+            <h2>I'am a <b>Functional Component</b></h2>
+            <p>I can generate random number like { generateRandomNumber() }</p>
+            <p>I can also use <b>props</b> like so:</p>
+            {/* creating a props called name and age */}
+            <p>My name is {props.name} and I am {props.age}</p>
+            <p>{props.children}</p>
+            <hr/>
+        </div>
+    )
 };
 
 export default person;
