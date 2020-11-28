5cc7238959ad311e70787e0ac5f7c86f8394dc31
valdal14 <valdal14@gmail.com>
2020-11-28 17:54:52 +0100

    Started the brief intro about hooks

--- 9-React-Complete-Project/react-project/src/App.js
+++ 9-React-Complete-Project/react-project/src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+// imported the useState Hook from React
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  // React state
-  state = {
+
+function App(props) {
+  // React hook for functional components
+  const [ personState, setPersonState ] = useState({
     person: [
       {
         id: 0,
@@ -17,12 +19,22 @@ class App extends Component {
         age: 45
       },
     ]
-  }
+  })
 
-  switchNameHandler = ()=> {
-    if (this.state.person[0].name === 'Valerio') {
-      this.setState({
-        // updating just person state
+  const [ secondState, setSecondState ] = useState({
+    otherState: 'This is another state'
+  })
+
+
+  const switchNameHandler = ()=> {
+    // loggin the state
+    console.log(personState);
+    console.log(secondState);
+
+    // get to the Hook state
+    if (personState.person[0].name === 'Valerio'){
+      // setting the new state
+      setPersonState({
         person: [
           {
             id: 0,
@@ -34,10 +46,13 @@ class App extends Component {
             name: 'Valerio',
             age: 42
           },
-        ]
+        ],
+        // Additional state
+        secondState: personState.secondState
       })
     } else {
-      this.setState({
+      // setting the new state
+      setPersonState({
         person: [
           {
             id: 0,
@@ -54,20 +69,87 @@ class App extends Component {
     }
   }
 
-  render(){
-    return (
-      <div className="App">
+  return(
+    <div className="App">
         {/* Change the state value using setState method */}
         {/* Never ever add () to the handler otherwise it will be called immediatly */}
-        <button onClick={this.switchNameHandler}>Switch Users</button>
+        <button onClick={switchNameHandler}>Switch Users</button>
 
         {/* Using state inside Components */}
-       <Person name={this.state.person[0].name} age={this.state.person[0].age} />
+       <Person name={personState.person[0].name} age={personState.person[0].age} />
        {/* Using Children props inside Components */}
-       <Person name={this.state.person[1].name} age={this.state.person[1].age} > My Hobby is: Reading</Person>
+       <Person name={personState.person[1].name} age={personState.person[1].age} > My Hobby is: Reading</Person>
       </div>
-    );
-  }
+  )
 }
 
 export default App;
+
+
+// class App extends Component {
+//   // React state
+//   state = {
+//     person: [
+//       {
+//         id: 0,
+//         name: 'Valerio',
+//         age: 42
+//       },
+//       {
+//         id: 1,
+//         name: 'Lefemas',
+//         age: 45
+//       },
+//     ]
+//   }
+
+//   switchNameHandler = ()=> {
+//     if (this.state.person[0].name === 'Valerio') {
+//       this.setState({
+//         // updating just person state
+//         person: [
+//           {
+//             id: 0,
+//             name: 'Lefemas',
+//             age: 45
+//           },
+//           {
+//             id: 1,
+//             name: 'Valerio',
+//             age: 42
+//           },
+//         ]
+//       })
+//     } else {
+//       this.setState({
+//         person: [
+//           {
+//             id: 0,
+//             name: 'Valerio',
+//             age: 42
+//           },
+//           {
+//             id: 1,
+//             name: 'Lefemas',
+//             age: 45
+//           },
+//         ]
+//       })
+//     }
+//   }
+
+//   render(){
+//     return (
+//       <div className="App">
+//         {/* Change the state value using setState method */}
+//         {/* Never ever add () to the handler otherwise it will be called immediatly */}
+//         <button onClick={this.switchNameHandler}>Switch Users</button>
+
+//         {/* Using state inside Components */}
+//        <Person name={this.state.person[0].name} age={this.state.person[0].age} />
+//        {/* Using Children props inside Components */}
+//        <Person name={this.state.person[1].name} age={this.state.person[1].age} > My Hobby is: Reading</Person>
+//       </div>
+//     );
+//   }
+// }
