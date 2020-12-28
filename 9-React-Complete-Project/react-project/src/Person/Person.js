361a138eaf3faecaa5388e2dc898d35a995f87ec
valdal14 <valdal14@gmail.com>
2020-12-28 16:39:47 +0100

    Completed the section about useEffect hook

--- 9-React-Complete-Project/react-project/src/Person/Person.js
+++ 9-React-Complete-Project/react-project/src/Person/Person.js
@@ -1,58 +1,70 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './Person.css';
 
 // React must be imported since the following will be converted in React.createElement and JSX
 // We do not need to import Component tho since we are not using a class
 
 // We must use props parameter inside our functional component to be able to create props.name and props.age
-class Person extends Component {
+// class Person extends Component {
 
-    constructor(props) {
-        super(props);
-        this.style = {
-            '@media (minWidth: 500px)' : {
-                width: '450px'
-            }
-        }
-    }
-
-    getSnapshotBeforeUpdate(prevProps, prevState){
-        console.log('Person-Component: getSnapshotBeforeUpdate - Props: ' + this.props + ' State: ' + null);
-        return {message: 'Snapshot of the props'};
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot){
-        console.log('Person-Component: componentDidUpdate - Props: ' + prevProps + ' State: ' + prevState + ' Snapshot: ' + snapshot);
-    }
-
-    render() {
-        return (
-            <div className='Person' style={this.style}>
-                { console.log('[Person] rendering...') }
-                <input type='text' onChange={ this.props.changeName } defaultValue={this.props.name} id={this.props.id} />
-                <p onClick={ this.props.deletePerson }>My name is {this.props.name} and I am {this.props.age}</p>
-                <p>{this.props.children}</p>
-            </div>
-        )
-    }
-    
-}
-// const person = (props) => {
-    
-//     const style = {
-//         '@media (minWidth: 500px)' : {
-//             width: '450px'
+//     constructor(props) {
+//         super(props);
+//         this.style = {
+//             '@media (minWidth: 500px)' : {
+//                 width: '450px'
+//             }
 //         }
-//     };
+//     }
+
+//     getSnapshotBeforeUpdate(prevProps, prevState){
+//         console.log('Person-Component: getSnapshotBeforeUpdate - Props: ' + this.props + ' State: ' + null);
+//         return {message: 'Snapshot of the props'};
+//     }
+
+//     componentDidUpdate(prevProps, prevState, snapshot){
+//         console.log('Person-Component: componentDidUpdate - Props: ' + prevProps + ' State: ' + prevState + ' Snapshot: ' + snapshot);
+//     }
+
+//     render() {
+//         return (
+//             <div className='Person' style={this.style}>
+//                 { console.log('[Person] rendering...') }
+//                 <input type='text' onChange={ this.props.changeName } defaultValue={this.props.name} id={this.props.id} />
+//                 <p onClick={ this.props.deletePerson }>My name is {this.props.name} and I am {this.props.age}</p>
+//                 <p>{this.props.children}</p>
+//             </div>
+//         )
+//     }
     
-//     return (
-//         <div className='Person' style={style}>
-//             { console.log('[Person] rendering...') }
-//             <input type='text' onChange={ props.changeName } defaultValue={props.name} id={props.id} />
-//             <p onClick={ props.deletePerson }>My name is {props.name} and I am {props.age}</p>
-//             <p>{props.children}</p>
-//         </div>
-//     )
-// };
+// }
+const Person = (props) => {
+    
+    useEffect(()=> {
+        setTimeout(()=>{
+            alert('useEffect executing - Data has been saved!!!');
+        }, 1000)
+
+        return ()=> {
+            // clean up with useEffect
+            console.log('cleaning up the Person component')
+        };
+
+    }, []);
+
+    const style = {
+        '@media (minWidth: 500px)' : {
+            width: '450px'
+        }
+    };
+    
+    return (
+        <div className='Person' style={style}>
+            { console.log('[Person] rendering...') }
+            <input type='text' onChange={ props.changeName } defaultValue={props.name} id={props.id} />
+            <p onClick={ props.deletePerson }>My name is {props.name} and I am {props.age}</p>
+            <p>{props.children}</p>
+        </div>
+    )
+};
 
 export default Person;
