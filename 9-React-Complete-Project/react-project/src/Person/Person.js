b9c39996afbdca5825a487454b1934fa7d928192
valdal14 <valdal14@gmail.com>
2020-12-30 10:37:31 +0100

    intro to HOC in react

--- 9-React-Complete-Project/react-project/src/Person/Person.js
+++ 9-React-Complete-Project/react-project/src/Person/Person.js
@@ -1,42 +1,7 @@
 import React, { useEffect, memo } from 'react';
 import './Person.css';
+import Aux from '../HOC/Aux';
 
-// React must be imported since the following will be converted in React.createElement and JSX
-// We do not need to import Component tho since we are not using a class
-
-// We must use props parameter inside our functional component to be able to create props.name and props.age
-// class Person extends Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.style = {
-//             '@media (minWidth: 500px)' : {
-//                 width: '450px'
-//             }
-//         }
-//     }
-
-//     getSnapshotBeforeUpdate(prevProps, prevState){
-//         console.log('Person-Component: getSnapshotBeforeUpdate - Props: ' + this.props + ' State: ' + null);
-//         return {message: 'Snapshot of the props'};
-//     }
-
-//     componentDidUpdate(prevProps, prevState, snapshot){
-//         console.log('Person-Component: componentDidUpdate - Props: ' + prevProps + ' State: ' + prevState + ' Snapshot: ' + snapshot);
-//     }
-
-//     render() {
-//         return (
-//             <div className='Person' style={this.style}>
-//                 { console.log('[Person] rendering...') }
-//                 <input type='text' onChange={ this.props.changeName } defaultValue={this.props.name} id={this.props.id} />
-//                 <p onClick={ this.props.deletePerson }>My name is {this.props.name} and I am {this.props.age}</p>
-//                 <p>{this.props.children}</p>
-//             </div>
-//         )
-//     }
-    
-// }
 const Person = (props) => {
     
     useEffect(()=> {
@@ -59,12 +24,15 @@ const Person = (props) => {
     };
     
     return (
-        <div className='Person' style={style}>
-            { console.log('[Person] rendering...') }
-            <input type='text' onChange={ props.changeName } defaultValue={props.name} id={props.id} />
-            <p onClick={ props.deletePerson }>My name is {props.name} and I am {props.age}</p>
-            <p>{props.children}</p>
-        </div>
+        // Note that now the key attribute is not required
+        <Aux>
+            <div className='Person' style={style}>
+                <input type='text' onChange={ props.changeName } defaultValue={props.name} id={props.id} />
+            </div>
+            <div className='Person' style={style}>
+                <p onClick={ props.deletePerson }>My name is {props.name} and I am {props.age}</p>
+            </div>
+        </Aux>
     )
 };
 
