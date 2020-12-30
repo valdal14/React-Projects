a3049b341551039ef4035f540d65405bfc76fdd8
valdal14 <valdal14@gmail.com>
2020-12-30 12:32:05 +0100

    React Refs added to the training section

--- 9-React-Complete-Project/react-project/src/Person/Person.js
+++ 9-React-Complete-Project/react-project/src/Person/Person.js
@@ -1,79 +1,84 @@
-import React, { useEffect, memo } from 'react';
+import React, { useEffect, memo, useRef } from 'react';
 import './Person.css';
 import Aux from '../HOC/Aux';
 import PropsType from 'prop-types';
 
-class Person extends React.Component {
+// class Person extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.style = {
-            '@media (minWidth: 500px)' : {
-                width: '450px'
-            }
-        }
-        // using createRef to bind the inputElement to the ref
-        this.inputElement = React.createRef();
-    }
-
-    componentDidMount(){
-        this.inputElement.current.focus();
-    }
-
-    render(){
-        return(
-            <Aux>
-            <div className='Person' style={this.style}>
-                <input type='text' 
-                onChange={ this.props.changeName } 
-                defaultValue={this.props.name} 
-                id={this.props.id}
-                ref={this.inputElement}
-                />
-                
-            </div>
-            <div className='Person' style={this.style}>
-                <p onClick={ this.props.deletePerson }>My name is {this.props.name} and I am {this.props.age}</p>
-            </div>
-        </Aux>
-        )
-    }
-}
-
-
-// const Person = (props) => {
-    
-//     useEffect(()=> {
-//         const timer = setTimeout(()=>{
-//             alert('useEffect executing - Data has been saved!!!');
-//         }, 1000)
-
-//         return ()=> {
-//             // clean up with useEffect
-//             clearTimeout(timer);
-//             console.log('cleaning up the Person component')
-//         };
-
-//     }, []);
-
-//     const style = {
-//         '@media (minWidth: 500px)' : {
-//             width: '450px'
+//     constructor(props) {
+//         super(props);
+//         this.style = {
+//             '@media (minWidth: 500px)' : {
+//                 width: '450px'
+//             }
 //         }
-//     };
-    
-//     return (
-//         // Note that now the key attribute is not required
-//         <Aux>
-//             <div className='Person' style={style}>
-//                 <input type='text' onChange={ props.changeName } defaultValue={props.name} id={props.id} />
+//         // using createRef to bind the inputElement to the ref
+//         this.inputElement = React.createRef();
+//     }
+
+//     componentDidMount(){
+//         this.inputElement.current.focus();
+//     }
+
+//     render(){
+//         return(
+//             <Aux>
+//             <div className='Person' style={this.style}>
+//                 <input type='text' 
+//                 onChange={ this.props.changeName } 
+//                 defaultValue={this.props.name} 
+//                 id={this.props.id}
+//                 ref={this.inputElement}
+//                 />
+                
 //             </div>
-//             <div className='Person' style={style}>
-//                 <p onClick={ props.deletePerson }>My name is {props.name} and I am {props.age}</p>
+//             <div className='Person' style={this.style}>
+//                 <p onClick={ this.props.deletePerson }>My name is {this.props.name} and I am {this.props.age}</p>
 //             </div>
 //         </Aux>
-//     )
-// };
+//         )
+//     }
+// }
+
+
+const Person = (props) => {
+    
+    const inputElement = useRef(null);
+
+    useEffect(()=> {
+        inputElement.current.focus()
+
+        // unmount the component
+        return ()=> {
+            console.log('Unmounting the Person Component');
+        };
+
+        // will execute just once
+    }, []);
+
+    const style = {
+        '@media (minWidth: 500px)' : {
+            width: '450px'
+        }
+    };
+    
+    return (
+        // Note that now the key attribute is not required
+        <Aux>
+            <div className='Person' style={style}>
+                <input type='text' 
+                onChange={ props.changeName } 
+                defaultValue={props.name} 
+                id={props.id} 
+                ref={inputElement}
+                />
+            </div>
+            <div className='Person' style={style}>
+                <p onClick={ props.deletePerson }>My name is {props.name} and I am {props.age}</p>
+            </div>
+        </Aux>
+    )
+};
 
 Person.propTypes = {
     changeName: PropsType.func,
